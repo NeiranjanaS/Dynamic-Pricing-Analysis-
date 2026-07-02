ad4b363aa2ac918eb8ee3a760984d0d1d065fd12
Miruthula S B <miruthula122@gmail.com>
2026-07-02 21:53:54 +0530

    Added comments

--- sql/advanced_queries.sql
+++ sql/advanced_queries.sql
@@ -1,18 +1,29 @@
 USE travel;
 
+-- Average ADR by Hotel Type
+
 SELECT hotel,ROUND(AVG(adr),2) AS avg_adr
 FROM hotel_bookings_cleaned
 GROUP BY hotel;
+
+
+-- Hotel types having average ADR greater than 100
 
 SELECT hotel,ROUND(AVG(adr),2) AS avg_adr
 FROM hotel_bookings_cleaned
 GROUP BY hotel
 HAVING AVG(adr) > 100;
 
+
+-- GROUP BY + Cancellation Analysis
+
 SELECT market_segment,COUNT(*) AS bookings,SUM(is_canceled) AS cancellations,ROUND(AVG(is_canceled)*100,2) AS cancellation_rate
 FROM hotel_bookings_cleaned
 GROUP BY market_segment
 ORDER BY cancellation_rate DESC;
+
+
+-- CTE
 
 WITH monthly_booking AS
 (
@@ -20,9 +31,11 @@ WITH monthly_booking AS
     FROM hotel_bookings_cleaned
     GROUP BY arrival_date_month
 )
-SELECT *
-FROM monthly_booking
+SELECT * FROM monthly_booking 
 ORDER BY bookings DESC;
+
+
+-- WINDOW FUNCTION : RANK()
 
 SELECT arrival_date_month,ROUND(AVG(adr),2) AS avg_adr,
     RANK() OVER(
@@ -31,12 +44,18 @@ SELECT arrival_date_month,ROUND(AVG(adr),2) AS avg_adr,
 FROM hotel_bookings_cleaned
 GROUP BY arrival_date_month;
 
+
+-- WINDOW FUNCTION : DENSE_RANK()
+
 SELECT market_segment,ROUND(AVG(adr),2) AS avg_adr,
     DENSE_RANK() OVER(
         ORDER BY AVG(adr) DESC
     ) AS segment_rank
 FROM hotel_bookings_cleaned
 GROUP BY market_segment;
+
+
+-- WINDOW FUNCTION : ROW_NUMBER()
 
 SELECT hotel,adr,
     ROW_NUMBER() OVER(
@@ -45,6 +64,9 @@ SELECT hotel,adr,
     ) AS row_num
 FROM hotel_bookings_cleaned;
 
+
+--WINDOW FUNCTION : LAG()
+
 WITH monthly_adr AS
 (
     SELECT arrival_date_year,arrival_date_month,ROUND(AVG(adr),2) AS avg_adr
@@ -52,6 +74,9 @@ WITH monthly_adr AS
     GROUP BY arrival_date_year,arrival_date_month
 )
 SELECT * FROM monthly_adr;
+
+
+-- Monthly ADR Analysis
 
 SELECT arrival_date_month,
 ROUND(AVG(adr),2) AS average_adr,
